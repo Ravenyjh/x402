12706b92768280e30274b6cfc1b62618e35b1224
Ravenyjh <jiahaoyan20172@gmail.com>
2025-10-26 18:58:33 +0800

    feat: add 0g Chain support

--- typescript/packages/x402/src/types/shared/evm/config.ts
+++ typescript/packages/x402/src/types/shared/evm/config.ts
@@ -52,6 +52,10 @@ export const config: Record<string, ChainConfig> = {
     usdcAddress: "0xbbA60da06c2c5424f03f7434542280FCAd453d10",
     usdcName: "USDC",
   },
+  "16661": {
+    usdcAddress: "0x1f3aa82227281ca364bfb3d253b0f1af1da6473e",
+    usdcName: "Bridged USDC",
+  },
 };
 
 export type ChainConfig = {
--- typescript/packages/x402/src/types/shared/network.ts
+++ typescript/packages/x402/src/types/shared/network.ts
@@ -13,6 +13,7 @@ export const NetworkSchema = z.enum([
   "polygon",
   "polygon-amoy",
   "peaq",
+  "0g-chain",
 ]);
 export type Network = z.infer<typeof NetworkSchema>;
 
@@ -28,6 +29,7 @@ export const SupportedEVMNetworks: Network[] = [
   "polygon",
   "polygon-amoy",
   "peaq",
+  "0g-chain",
 ];
 export const EvmNetworkToChainId = new Map<Network, number>([
   ["base-sepolia", 84532],
@@ -40,6 +42,7 @@ export const EvmNetworkToChainId = new Map<Network, number>([
   ["polygon", 137],
   ["polygon-amoy", 80002],
   ["peaq", 3338],
+  ["0g-chain", 16661],
 ]);
 
 // svm
